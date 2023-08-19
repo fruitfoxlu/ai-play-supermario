cd9f958e24420c871dadbb02ed5e9a9279099b40
Royce Lu <rlu@paloaltonetworks.com>
2023-08-18 22:14:39 -0700

    add new reward policy

--- dqnagent.py
+++ dqnagent.py
@@ -1,6 +1,8 @@
 import numpy as np
 import tensorflow as tf
 import gym
+import copy
+
 
 class DQNAgent:
     def __init__(self, input_shape, action_size):
@@ -13,7 +15,11 @@ class DQNAgent:
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001
         self.model = self._build_model()
-        self.new_lesson()  # Initialize to an invalid value
+        self.prev_x_position = 0
+        self.prev_y_position = 0
+        self.biggest_jump_x = 0
+        self.biggest_jump_y = 0
+        #self.new_lesson()  # Initialize to an invalid value
 
 
     def _build_model(self):
@@ -23,6 +29,10 @@ class DQNAgent:
         model.add(tf.keras.layers.Conv2D(32, (8, 8), strides=(4, 4), padding='valid', activation='relu', input_shape=self.input_shape))
         model.add(tf.keras.layers.Conv2D(64, (4, 4), strides=(2, 2), padding='valid', activation='relu'))
         model.add(tf.keras.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='valid', activation='relu'))
+
+        # Pooling layer to reduce spatial dimensions
+        model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+
         model.add(tf.keras.layers.Flatten())
         
         # Fully connected layers
@@ -32,42 +42,86 @@ class DQNAgent:
         model.compile(loss='mse', optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate))
         return model
 
-    def new_lesson(self):
+    def new_lesson(self,state):
         self.prev_x_position = -1  # Initialize to an invalid value
-
+        # Initialization before starting the episode
+        self.state_buffer = [state]*4
+        self.stacked_buffer = np.stack(self.state_buffer, axis=0)
+        
+   
     def remember(self, state, action, info,reward, next_state, done):
         shapped_reward = self.reward_shaping(info,reward)
-        self.memory.append((state, action, shapped_reward, next_state, done))
+        old_buffer = copy.deepcopy(self.stacked_buffer)
+       
+        # Update the buffer with the new state and drop the oldest state
+        self.state_buffer.pop(0)
+        self.state_buffer.append(next_state)
+
+        # Stack the states from the buffer to form the input for the neural network
+        self.stacked_buffer = np.stack(self.state_buffer, axis=0)
+        #print("last 4 stack")
+        #print(self.stacked_buffer.shape)
+        
+        self.memory.append((old_buffer,action, shapped_reward, self.stacked_buffer , done))
+
+   
+
+    def predict_with_current_frame(self):
+        
+        return self.model.predict(self.stacked_buffer)
+
+    def predict_with_state(self,state):
+            
+        return self.model.predict(state) 
+        
 
     def act(self, state):
+        #chose random actions at the first few rounds of the training 
+       
         if np.random.rand() <= self.epsilon:
-            return np.random.randint(self.action_size)
-        if state.shape[1:] != (240, 256, 4):
-            print(f"act : Skipped prediction due to shape mismatch: {state.shape}")
-            return 0
-        act_values = self.model.predict(state)
+             act_values = np.random.randint(self.action_size)
+             #print(f"random act {act_values} {np.random.rand()} {self.epsilon}")
+             return act_values
+       
+        act_values =  self.predict_with_current_frame()
+        print(f"act {act_values}")
         return np.argmax(act_values[0])
 
     def reward_shaping(self,info,reward):
         # If Mario's x position hasn't changed, modify the reward
-        # if self.prev_x_position <= info['x_pos']:
-        #     print("Penalize for not moving forward")
-        #     reward += 0.1  # Penalize for not moving forward
+        #print(info)
+        if self.prev_x_position <= info['x_pos']:
+             #print("prize for not moving forward")
+             reward += 0.2  # Penalize for not moving forward
+        if info['y_pos'] - self.prev_y_position > 29:
+             print("prize for jumping high")
+             reward += 0.2
+        jump_high = info['y_pos'] - self.prev_y_position
+        if (jump_high > self.biggest_jump_y):
+            print("highest jump %d"%jump_high)
+            self.biggest_jump_y = jump_high
+        self.prev_x_position = info['x_pos']
+        self.prev_y_position = info['y_pos']
+
         return reward
 
     def replay(self, batch_size):
         minibatch_indices = np.random.choice(len(self.memory), batch_size, replace=False)
         minibatch = [self.memory[index] for index in minibatch_indices] 
         for state, action, reward, next_state, done in minibatch:
-            if (next_state.shape[1:] != (240, 256, 4) or state.shape[1:] != (240, 256, 4)):
-                print(f"replay : Skipped prediction due to shape mismatch: {next_state.shape}")
+            if (next_state.shape[1:] != (240, 256,3) or state.shape[1:] != (240, 256,3)):
+                #print(f"replay : Skipped prediction due to shape mismatch: {next_state.shape}")
                 continue 
+            
             target = reward
             # Check the shape of next_state
             if not done :
+                #print("replay")
+                #print(next_state.shape)
                 target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
-            
-            target_f = self.model.predict(state)
+
+            #here we update the current predict action to the new action (target??) 
+            target_f = self.predict_with_state(state)
             target_f[0][action] = target
             self.model.train_on_batch(state, target_f)
 
--- play_mario.py
+++ play_mario.py
@@ -13,8 +13,8 @@ MY_SIMPLE_MOVEMENT = [
     ['right', 'B'],
     ['right', 'A', 'B'],
     ['A'],
-    ['A','A'],
     ['A','A','A'],
+    ['A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A'],
     ['A','right', 'A'],
     ['left']
 ]
@@ -40,25 +40,27 @@ env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, MY_SIMPLE_MOVEMENT)  # Use SIMPLE_MOVEMENT to reduce action space
 
 # Create the DQN agent
-state_size = (240, 256, 4)  # 4 stacked frames of 240x256 pixels
+state_size = (240, 256, 3)  #  240x256X3 for each frame
 action_size = env.action_space.n
 agent = DQNAgent(state_size, action_size)  # You might need to modify DQNAgent to handle image input
 
 # Training loop
 episodes = 1000
-batch_size = 32
+batch_size = 16
 for e in range(episodes):
     state = env.reset()
-    state = np.stack([state] * 4, axis=2)  # Stack 4 frames
+    agent.new_lesson(state)
+
     total_reward = 0
     done = False
 
     
     while not done:
         env.render() # This will display the game screen
+
         action = agent.act(state)
         next_state, reward, done, info = env.step(action)
-        next_state = np.stack([next_state] * 4, axis=2)
+       
         agent.remember(state, action, info,reward, next_state, done)
         
         state = next_state
@@ -66,5 +68,5 @@ for e in range(episodes):
         if done:
             print(f"Episode: {e}/{episodes}, Score: {total_reward}")
             break
-    if len(agent.memory) > batch_size:
+    if len(agent.memory) > batch_size: #kick-off learning after one episodes
         agent.replay(batch_size) #update the NN by so far what we learn
